4d2edf059693583ef53c11adc931546d1c58c600
MIKSU995 <fahmihartanto6@gmail.com>
2026-08-04 20:08:06 +0700

    Merge remote and local repository

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,23 +5,24 @@ namespace App\Http\Controllers;
 use App\Models\Media;
 use App\Models\Genre;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index()
-{
-    try {
-        DB::connection()->getPdo();
+    {
+        try {
+            DB::connection()->getPdo();
 
-        dd([
-            'status' => 'CONNECTED',
-            'tables' => DB::select('SHOW TABLES'),
-        ]);
-    } catch (\Exception $e) {
-        dd([
-            'status' => 'FAILED',
-            'error' => $e->getMessage(),
-        ]);
+            dd([
+                'status' => 'CONNECTED',
+                'tables' => DB::select('SHOW TABLES'),
+            ]);
+        } catch (\Exception $e) {
+            dd([
+                'status' => 'FAILED',
+                'error' => $e->getMessage(),
+            ]);
+        }
     }
-}
 }
