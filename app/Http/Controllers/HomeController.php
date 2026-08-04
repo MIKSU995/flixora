279a21c3ff67066e2575359944ae0c753b6dc163
MIKSU995 <fahmihartanto6@gmail.com>
2026-08-04 20:22:05 +0700

    Merge remote and local repository

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,12 +9,50 @@ use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-   public function index()
+   public function index(Request $request)
 {
-    $featured = Media::orderByDesc('avg_rating')->take(5)->get();
+    $searchQuery = $request->search;
+    $selectedGenreSlug = $request->genre;
+    $selectedType = $request->type;
 
-    $latest = Media::latest()->take(12)->get();
+    $featuredMedia = Media::orderByDesc('avg_rating')->first();
 
-    return view('home', compact('featured', 'latest'));
+    $topRatedMedia = Media::with('genres')
+        ->orderByDesc('avg_rating')
+        ->take(6)
+        ->get();
+
+    $query = Media::with('genres');
+
+    if ($searchQuery) {
+        $query->where('title', 'like', "%{$searchQuery}%");
+    }
+
+    if ($selectedType) {
+        $query->where('type', $selectedType);
+    }
+
+    if ($selectedGenreSlug) {
+        $query->whereHas('genres', function ($q) use ($selectedGenreSlug) {
+            $q->where('slug', $selectedGenreSlug);
+        });
+    }
+
+    $catalogMedia = $query
+        ->latest()
+        ->paginate(12)
+        ->withQueryString();
+
+    $allGenres = Genre::orderBy('name')->get();
+
+    return view('home', compact(
+        'featuredMedia',
+        'topRatedMedia',
+        'catalogMedia',
+        'allGenres',
+        'searchQuery',
+        'selectedGenreSlug',
+        'selectedType'
+    ));
 }
 }
