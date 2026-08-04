71d2cdf52e845267e8d6fd52f614226041cc9123
MIKSU995 <fahmihartanto6@gmail.com>
2026-08-04 19:35:30 +0700

    Merge remote and local repository

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,18 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Media;
-use App\Models\Genre;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(Request $request)
-{
-    dd(
-        config('database.default'),
-        DB::connection()->getDatabaseName(),
-        Media::count()
-    );
-}
+    {
+        dd([
+            'database' => DB::connection()->getDatabaseName(),
+            'db_count' => DB::table('media')->count(),
+            'eloquent_count' => Media::count(),
+            'titles' => Media::pluck('title'),
+        ]);
+    }
 }
